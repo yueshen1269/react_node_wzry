dcb65166c1af056af441e1d1757cebaccaf570b2
yueshen1269 <1152555377@qq.com>
2019-12-18 08:41:55 +0800

    TS-handbook 高级类型学习

--- admin/src/components/Typescript/test.tsx
+++ admin/src/components/Typescript/test.tsx
@@ -164,7 +164,6 @@ abstract class Department {
   // 抽象类中的抽象方法不包含具体实现并且必须在派生类中实现
   abstract printMeeting(): void; // 必须在派生类中实现
 }
-
 class AccountingDepartment extends Department {
   constructor() {
     super("Accounting and Auditing"); // 在派生类的构造函数中必须调用 super()
@@ -182,6 +181,259 @@ department = new AccountingDepartment(); // 允许对一个抽象子类进行实
 department.printName();
 department.printMeeting();
 // department.generateReports(); // 错误: 方法在声明的抽象类中不存在
+
+// 类型兼容的基本规则：如果x要兼容y，那么y至少具有与x相同的属性
+interface Named {
+  name: string;
+}
+let nameX: Named;
+let nameY = { name: "Jack", age: 21 };
+// nameX兼容nameY，但nameX不可以直接赋值{name: "Jack", age: 21}
+nameX = nameY;
+// 检查函数参数时使用相同的规则
+function greetNameD(n: Named) {
+  return n.name;
+}
+greetNameD(nameY);
+
+// 交叉类型intersection
+function extend<T, U>(first: T, second: U): T & U {
+  let result = {} as T & U;
+  for (let id in first) {
+    (result as any)[id] = (first as any)[id];
+  }
+  for (let id in second) {
+    if (!result.hasOwnProperty(id)) {
+      (result as any)[id] = (second as any)[id];
+    }
+  }
+  return result;
+}
+// 联合类型Union
+interface Bird {
+  fly();
+  layEggs();
+}
+interface Fish {
+  swim();
+  layEggs();
+}
+function getSmallPet(): Fish | Bird {
+  // ...
+  return {} as Fish | Bird;
+}
+let pet = getSmallPet();
+//  联合类型 只能访问所有类型中共有的成员
+pet.layEggs(); // okay
+// pet.swim();    // errors
+
+// 类型保护与区分类型 Type Guards and Differentiating Types
+// 多次使用类型断言，很冗余
+if ((pet as Fish).swim) {
+  (pet as Fish).swim();
+} else {
+  (pet as Bird).fly();
+}
+// 类型保护机制，会在运行时检查以确保在某个作用域里的类型，定义一个类型保护，需要定义一个函数，返回值是一个类型谓词
+// pet is Fish就是类型谓词，谓词为parameterName is Type 这种形式
+// 每当使用一些变量调用isFish是，TypeScript会将变量缩减为那个具体的类型
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+// TS不仅知道if分支里pet类型为Fish，而且还知道else分支里pet类型一定为Bird
+if (isFish(pet)) {
+  pet.swim();
+} else {
+  pet.fly();
+}
+
+// typeof 类型保护
+// 对于原始类型来说，必须定义一个函数来判断其类型太痛苦了。不必将typeof x === "typename"抽象成一个函数，TS可以将它识别为一个类型保护，
+// 直接在代码里检查类型了
+// ps: typename 必须是"number","string","boolean","symbol"
+function padLeft(value: string, padding: string | number) {
+  if (typeof padding === "number") {
+    return Array(padding + 1).join(" ") + value;
+  }
+  if (typeof padding === "string") {
+    return padding + value;
+  }
+  throw new Error(`excepted string or number`);
+}
+
+// instanceof类型保护
+// instanceof 右侧要求是一个构造函数，TS将细化为：
+// 1. 此构造函数prototype属性的类型，如果不为any
+// 2. 构造签名所返回的类型的联合
+function getRandomClass() {
+  return Math.random() < 0.5 ? new AnimalWithPrivateName("animal") : new RhinoProtected();
+}
+let randomAnimalClass = getRandomClass();
+if (randomAnimalClass instanceof AnimalWithPrivateName) {
+  randomAnimalClass;
+} else {
+  randomAnimalClass;
+}
+
+// null和undefined 可以赋值给任何类型，是其他类型的一个有效值，意味着不能阻止将它们赋值给其他类型
+// --strictNullChecks 标记可以解决此错误，声明一个变量时不会自动的包含null和undefined
+let s = "foo";
+// s = null; // 错误, 'null'不能赋值给'string'
+let sn: string | null = "bar";
+sn = null; // 可以
+// 使用 --strictNullChecks，可选参数和可选属性会被自动的加上 | undefined
+function fOption(x: number, y?: number) {
+  return x + (y || 0);
+}
+fOption(1, 2);
+fOption(1, undefined);
+// fOption(1,null); // error, 'null' is not assignable to 'number | undefined'
+
+// 判断null或undefined
+function fNull(sn: string | null): string {
+  if (sn === null) {
+    return "default";
+  } else {
+    return sn;
+  }
+  // 或者 return sn || "default";
+}
+// 可以使用类型断言手动取出null，方法是添加!后缀, 如 identifier!
+
+// 类型别名，类似于接口，但可以作用于原始值，联合类型，元祖以及其他任何需要手写的类型
+type stringName = string;
+type NameResolver = () => string;
+type NameOrResolver = stringName | NameResolver;
+function getTypeName(n: NameOrResolver): Name {
+  if (typeof n === "string") {
+    return n;
+  } else {
+    return n();
+  }
+}
+// 类型别名也可以是泛型
+type Container<T> = { value: T };
+// 类型别名不能出现在声明右侧的任何地方
+// type Yikes = Array<Yikes>; // error
+
+// 字符串字面量
+type Easing = "ease-in" | "ease-out" | "ease-in-out";
+// 字符串字面量类型还可以用于区分函数重载
+function createElement(tagName: "img"): HTMLImageElement;
+function createElement(tagName: "input"): HTMLInputElement;
+// ... more overloads ...
+function createElement(tagName: string): Element {
+  // ... code goes here ...
+}
+// 可辨识联合（Discriminated Unions）,也称做标签联合或代数数据类型
+// 合并单例类型，联合类型，类型保护和类型别名来创建一个叫做 可辨识联合的高级模式
+interface Square {
+  kind: "square";
+  size: number;
+}
+interface Rectangle {
+  kind: "rectangle";
+  width: number;
+  height: number;
+}
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+type AllShape = Square | Rectangle | Circle;
+// 使用可辨识联合:
+function area(s: AllShape) {
+  switch (s.kind) {
+    case "square":
+      return s.size * s.size;
+    case "rectangle":
+      return s.height * s.width;
+    case "circle":
+      return Math.PI * s.radius ** 2;
+  }
+}
+// 当没有涵盖所有可辨识联合的变化时，我们想让编译器可以通知我们
+// 两种方式可以实现。 首先是启用 --strictNullChecks并且指定一个返回值类型：
+function area1(s: AllShape): number {
+  // error: returns number | undefined
+  switch (s.kind) {
+    case "square":
+      return s.size * s.size;
+    case "rectangle":
+      return s.height * s.width;
+    case "circle":
+      return Math.PI * s.radius ** 2;
+  }
+}
+// 第二种方法使用 never类型，编译器用它来进行完整性检查：
+// 1.总是会抛出异常或根本就不会有返回值的函数表达式或箭头函数表达式的返回值类型。
+// 2.变量也可能是Never类型，当它们被永不为真的类型保护所约束时。
+// Never类型是任何类型的子类型，可以赋值给任何类型；没有类型是Never的子类型或可以赋值给Never类型（除了Never本身）。 即使 Any类型也不可以赋值给Never类型。
+function assertNever(x: never): never {
+  throw new Error("Unexpected object: " + x);
+}
+function areaWithNever(s: AllShape) {
+  switch (s.kind) {
+    case "square":
+      return s.size * s.size;
+    case "rectangle":
+      return s.height * s.width;
+    case "circle":
+      return Math.PI * s.radius ** 2;
+    default:
+      return assertNever(s); // error here if there are missing cases
+  }
+}
+
+// 索引类型（Index types）使用索引类型，编译器就能够检查使用了动态属性名的代码
+
+// 通过 索引类型查询和 索引访问操作符：
+function pluck<T, K extends keyof T>(o: T, names: K[]): T[K][] {
+  return names.map(n => o[n]);
+}
+interface Person {
+  name: string;
+  age: number;
+}
+let person: Person = {
+  name: "Jarid",
+  age: 35,
+};
+let strings: string[] = pluck(person, ["name"]); // ok, string[]
+// keyof T， 索引类型查询操作符。 对于任何类型 T， keyof T的结果为 T上已知的公共属性名的联合。 例如：
+let personProps: keyof Person; // 'name' | 'age'
+// T[K]， 索引访问操作符
+// getProperty里的 o: T和 name: K，意味着 o[name]: T[K]。 当你返回 T[K]的结果，编译器会实例化键的真实类型，因此 getProperty的返回值类型会随着你需要的属性改变。
+function getProperty<T, K extends keyof T>(o: T, name: K): T[K] {
+  return o[name]; // o[name] is of type T[K]
+}
+let name: string = getProperty(person, "name");
+let age: number = getProperty(person, "age");
+// let unknown = getProperty(person, 'unknown'); // error, 'unknown' is not in 'name' | 'age'
+// 索引类型和字符串索引签名
+// keyof和 T[K]与字符串索引签名进行交互。 如果你有一个带有字符串索引签名的类型，那么 keyof T会是 string。 并且 T[string]为索引签名的类型：
+interface Map<T> {
+  [key: string]: T;
+}
+let keys: keyof Map<number>; // string
+let value: Map<number>["foo"]; // number
+
+// 映射类型 改变已知属性的每个属性类型，如将每个类型都变为可选的或者只读的
+type Readonly<T> = {
+  readonly [P in keyof T]: T[P];
+};
+type Partial<T> = {
+  [P in keyof T]?: T[P];
+};
+type Pick<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+type Record<K extends string, T> = {
+  [P in K]: T;
+};
+type PersonPartial = Partial<Person>;
+type ReadonlyPerson = Readonly<Person>;
+type ThreeStringProps = Record<"prop1" | "prop2" | "prop3", string>;
 
 type AppProps = { message: string };
 enum Job {
