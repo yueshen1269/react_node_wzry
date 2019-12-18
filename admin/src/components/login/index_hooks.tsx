538e3ea1a051f8bc74e9b0c2920f15b9697a45f5
yueshen1269 <1152555377@qq.com>
2019-12-18 16:02:20 +0800

    使用TS改写Login组件

--- admin/src/components/login/index_hooks.tsx
+++ admin/src/components/login/index_hooks.tsx
@@ -1,14 +1,93 @@
-import React, { useEffect } from "react";
-import { Card, Form, message, Button, Input } from "antd";
+import React, { useEffect, forwardRef, useImperativeHandle, ReactElement } from "react";
+import { Card, Form, Button, Input } from "antd";
+import { FormComponentProps } from "antd/es/form";
 
-function Login(props: any) {
+interface LoginProps extends FormComponentProps {
+  history: any;
+  location: any;
+  match: any;
+  login: (...args: any[]) => void;
+}
+type Ref = FormComponentProps;
+const Login = forwardRef<Ref, LoginProps>(({ form, location, login }: LoginProps, ref) => {
+  // useImperativeHandle(ref, () => ({
+  //   form,
+  // }));
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    form.validateFields(async (err, values) => {
+      if (!err) {
+        const _state = location.state;
+        let from = null;
+        if (_state) from = _state.from;
+        login(Object.assign({}, values, { from }));
+      }
+    });
+  };
+  const formItemLayout = {
+    labelCol: {
+      xs: { span: 5 },
+      sm: { span: 3 },
+    },
+    wrapperCol: {
+      xs: { span: 8 },
+      sm: { span: 8 },
+    },
+  };
+  const formTailLayout = {
+    // labelCol: { span: 2 },
+    wrapperCol: { span: 2, offset: 2 },
+  };
+  return (
+    <Card
+      title="login"
+      style={{
+        width: 500,
+        marginTop: "10rem",
+        marginLeft: "auto",
+        marginRight: "auto",
+      }}
+    >
+      <Form {...formItemLayout} onSubmit={handleSubmit}>
+        <Form.Item label="名称">
+          {form.getFieldDecorator("username", {
+            rules: [
+              {
+                required: true,
+                message: "请输入名称",
+              },
+            ],
+            initialValue: "",
+          })(<Input />)}
+        </Form.Item>
+        <Form.Item label="密码">
+          {form.getFieldDecorator("password", {
+            rules: [
+              {
+                required: true,
+                message: "请输入密码",
+              },
+            ],
+            initialValue: "",
+          })(<Input type="password" />)}
+        </Form.Item>
+        <Form.Item {...formTailLayout}>
+          <Button type="primary" htmlType="submit">
+            登录
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
+});
+function Login1(props) {
   useEffect(() => {
     if (window.localStorage.getItem("token")) {
       props.history.push("/");
     }
   }, []);
 
-  function handleSubmit(e) {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     props.form.validateFields(async (err, values) => {
       if (!err) {
@@ -78,4 +157,4 @@ function Login(props: any) {
   );
 }
 
-export default Form.create()(Login);
+export default Form.create<LoginProps>()(Login);
